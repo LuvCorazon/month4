e394aefbc769f4d7cee08e6f1701efff0ee9c892
Rustam Yuburov <rt138328@gmail.com>
2025-02-10 16:54:29 +0600

    homework 2

--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render, HttpResponse
 import random
 
+import posts
+from posts.models import Post
+
+
 def test_view(request):
     return HttpResponse(f"twinkling watermelon {random.randint(1, 100)}")
 
 def html_view(request):
     return render(request,"main.html")
+
+def post_list_view(request):
+    posts = (Post.objects.all())
+    print(posts)
+    return render(request,"posts/post_list.html",{"posts":posts})
+
+def post_detail_view(request, post_id):
+    post = Post.objects.get(id=post_id)
+    return render(request,"posts/post_detail.html",{"posts":posts})
